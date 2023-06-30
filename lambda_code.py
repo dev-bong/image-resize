cdd15e5ae64f586eefd35a0f7d93384b708ba1da
dev-bong <qhddnjswo011@gmail.com>
2023-06-30 12:40:07 +0900

    feat: 4:3 비율로 이미지 크롭 기능 추가
    - 이미지를 4:3 비율에 맞춰 자르거나, 단색 이미지를 붙이는 기능 추가
    - 이미지를 4:3 비율로 맞춘 후 1600, 1200 으로 리사이징

--- lambda_code.py
+++ lambda_code.py
@@ -6,6 +6,24 @@ from PIL import Image
 
 s3 = boto3.client("s3")
 
+def image_ratio_4_to_3(img):
+    # 이미지 4:3으로 만들기
+    w, h = (img.width, img.height)
+    w_43 = int(round(h * 1.333, 0))
+    print(w, h, w_43)
+
+    if w > w_43:  # 가로가 긴 경우, 양 옆 자르기
+        crop_size = (w - w_43)//2
+        img_43 = img.crop((crop_size, 0, w-crop_size, h))
+    elif w < w_43:  # 세로가 긴 경우, 양 옆 채우기
+        paste_img = Image.new("RGB", (w_43,h), "#d3d3d3")
+        paste_img.paste(img, ((w_43//2) - (w//2),0))
+        img_43 = paste_img
+    else:
+        img_43 = img
+    
+    return img_43
+
 def lambda_handler(event, context):
     records = event.get("Records")
     download_dir = "/tmp/"
@@ -22,7 +40,6 @@ def lambda_handler(event, context):
         print(name, ext)
         
         file_name = f"{name}.{ext}"
-        resize_file_name = f"{name}_low.{ext}"
         key = f"{ref_cd}/{name}.{ext}"
 
         with open(download_dir + file_name, "wb") as img:
@@ -31,24 +48,25 @@ def lambda_handler(event, context):
         
         with open(download_dir + file_name, "rb") as img:
             try:
-                img_resized = Image.open(img)
+                origin_img = Image.open(img)
             except:  # 이미지 파일이 아닐 경우
                 print("no image file!")
                 return {
                     "statusCode": 409,
                     "body": json.dumps("no image file")
                 }
-            print(f"original img size : {img_resized.size}")
-            # 일단 120 x 120으로 맞추도록 설정
-            r_width = 120 if img_resized.width >= 120 else img_resized.width
-            r_height = 120 if img_resized.height >= 120 else img_resized.height
+            print(f"original img size : {origin_img.size}")
 
-            print(f"resized img size : ({r_width}, {r_height})")
-            img_resized = img_resized.resize((r_width, r_height))
-            img_resized.save(download_dir + resize_file_name)
-        
-        with open(download_dir + resize_file_name, "rb") as img:
-            s3.upload_fileobj(img, resize_bucket, key)
+            ratio_43_img = image_ratio_4_to_3(origin_img)
+            print(origin_img.width, origin_img.height)
+
+            resize_file_name = f"{name}.{ext}"
+            resize_key = f"{ref_cd}/{resize_file_name}"
+            resized_img = ratio_43_img.resize((1600, 1200))
+            resized_img.save(download_dir + resize_file_name)
+
+            with open(download_dir + resize_file_name, "rb") as img:
+                s3.upload_fileobj(img, resize_bucket, resize_key)
     
     
     return {
